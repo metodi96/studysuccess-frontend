1cb4371d5376b7b657452206086cf7edbb96b517
Snezhina Milusheva <snezhina.milusheva@tum.de>
2020-06-12 13:20:27 +0300

    login with validation, no jwt yet

--- src/views/SignUpView.js
+++ src/views/SignUpView.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import SignUp from '../components/SignUp'
+import LoginForm from '../components/LoginForm'
 
 function SignUpView() {
     return (
         <div>
+            <LoginForm></LoginForm>
             <SignUp></SignUp>
         </div>
     )
