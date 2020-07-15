2bd4af66e240f6ef2274d6369d6c85f288d84ec9
DESKTOP-TIU6GNM\Metodi <metodi.manov@tum.de>
2020-07-16 01:10:10 +0200

    optimize search by making it display the value

--- src/views/TutorsView.js
+++ src/views/TutorsView.js
@@ -1,30 +1,21 @@
-import React, {useState, useEffect} from 'react'
-//import Filters from '../components/Filters'
-//import React  from 'react'
-//import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-//import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React, { useState } from 'react'
 import Filters from '../components/Filters'
 import TutorsList from '../components/TutorsList'
-import {TutorsContext} from '../components/TutorsContext'
+import { TutorsContext } from '../components/TutorsContext'
 import Search from '../components/Search'
 import Box from '@material-ui/core/Box';
 
 
-function TutorsView({match}) {
+function TutorsView({ match }) {
     const [tutorsForSubject, setTutorsForSubject] = useState([]);
-
     return (
         <Box>
-            <Search subjectId={match.params.subjectId}></Search>
-            <Box style={{display: "flex", minWidth: '1280px'}} mt="1%">
-                <TutorsContext.Provider value={{tutorsForSubject, setTutorsForSubject}}>
+            <Search subjectIdParam={match.params.subjectId}></Search>
+            <Box style={{ display: "flex", minWidth: '1280px' }} mt="1%">
+                <TutorsContext.Provider value={{ tutorsForSubject, setTutorsForSubject }}>
                     <Filters subjectId={match.params.subjectId}></Filters>
                     <TutorsList subjectId={match.params.subjectId}></TutorsList>
-                </TutorsContext.Provider>            
+                </TutorsContext.Provider>
             </Box>
         </Box>
     )
